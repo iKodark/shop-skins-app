51143b5f406a2a1cfbaad1d4d34ec3820c82ebe1
feer.henrique18@gmail.com <feer.henrique18@gmail.com>
2023-04-19 13:27:44 -0300

    Replace tags html img and a

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Image from 'next/image'
+import Link from 'next/link';
 
 const Profile = ({ user }: { user: any }) => {
   const { avatarfull, personaname } = user?._json || {avatarfull: null, personaname: null};
@@ -16,10 +17,10 @@ const Profile = ({ user }: { user: any }) => {
           </>
         ) :
         (
-          <a href="/api/auth/login" className="flex items-center justify-center gap-2 bg-slate-900 hover:bg-slate-800 duration-300 text-white p-3 rounded font-semibold">
+          <Link href="/api/auth/login" className="flex items-center justify-center gap-2 bg-slate-900 hover:bg-slate-800 duration-300 text-white p-3 rounded font-semibold">
             <Image className="fill-cyan-500 w-4" width={200} height={200} src="/valve.svg" alt="Valve" />
             Login
-          </a>
+          </Link>
         )
       }
     </>
